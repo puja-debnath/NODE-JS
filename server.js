fe6afc782101c7ef32d5dfafe4b071b8b4c7f3e9
unknown <104721799+puja-debnath@users.noreply.github.com>
2024-01-03 23:41:44 +0530

    web server update

--- server.js
+++ server.js
@@ -9,21 +9,56 @@ const EventEmitter = require("events")
 class Emitter extends EventEmitter{}
 const myEmitter = new Emitter()
 
-const server = http.createServer((req,res) =>{
-    console.log(req.url, req.method)
-    let letpath;
-    switch(req.url){
-        case"/" :
-        res.statusCode =200
-        path = path.join(__dirname,"views","index.html")
-        fs.readFile(path,"utf8",(err,data) =>{
-           res.end(data)
-        })
-        break;
+const server = http.createServer((req,res)=>{
+    console.log(req.url , req.method)
+
+    const extension = path.extname(req.url)
+  let contentType;
+    switch(extension){
+        case ".css":
+            contentType= "text/css";
+            break;
+        case ".js":
+            contentType= "text/javascript";
+            break;
+        case ".json":
+            contentType= "application/json";
+            break;
+        case ".jpg":
+            contentType= "image/jpeg";
+             break;
+        case ".png":
+            contentType= "image/png";
+            break;
+         case "txt":
+             contentType= "text/plain";
+              break;
+        default:
+             contentType= "text/html";
     }
-})
+
+    let filepath = 
+      contentType === "text/html" && req.url=== "/"
+      ? path.join(__dirname,"views","index.html")
+      :contentType === "text/html" && req.url.slice(-1) === "/"
+         ?path.join(__dirname,"views", req.url , "index.html")
+         :contentType === "text/html"
+              ?path.join(__dirname,"views", req.url)
+             :path.join(__dirname,req.url)
+    // males .html extension not required fpr browser
+    if(!extension && req.url.slice(-1) !== "/" ) filepath += ".html";
+
+    const fileExist = fs.existsSync(filepath)
+    if(fileExist){
+        //serve the file
+    }else{
+        //404
+       // 301redirect
+       console.log(path.parse(filepath))
+    }
+    
+});
+
+server.listen(PORT, () =>{console.log(`server runnning on ${PORT}`)})
 
 
-server.listen(PORT, () =>{
-    console.log(`server runnning on ${PORT}`)
-})
